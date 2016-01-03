d17b0270df171f4f1709343e57ab177f6bf8878f
yhomanez <yhomanez@gmail.com>
2016-01-03 20:22:11 +0700

    js remove func

--- assets/resource/js/myscript.js
+++ assets/resource/js/myscript.js
@@ -19,25 +19,3 @@ jQuery(document).ready(function () {
 		});
 	});
 });
-
-jQuery(document).ready(function () {
-	$(document).on('click','.j-save-merk', function(){
-		var url = $(this).attr("data-url");
-		var homeurl = $(this).attr("data-homeurl");
-		var namamerk = $("#namajenis").val();
-		$.ajax({
-			url : url,
-			type : 'POST',
-			data : {namamerk : namamerk,},
-			dataType : 'JSON',
-			success : function(data){
-				if(data.status === true){					
-					alert(data.msg);
-					window.location.href = homeurl;
-				}else{
-					alert(data.msg);
-				}
-			}
-		});
-	});
-});
